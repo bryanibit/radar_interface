cf7457ab51cf3897b2a54c3435439b326fd5522c
Juraj Persic <juraj.persic@fer.hr>
2018-05-14 13:13:39 +0200

    subscriber_publisher class added

--- src/vehicle/imu_to_vehicle_node.cpp
+++ src/vehicle/imu_to_vehicle_node.cpp
@@ -1,25 +1,34 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-
-
-void imuCallback(const std_msgs::String::ConstPtr& msg)
-{
-	ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
-
 int main(int argc, char *argv[])
 {
 
 	ros::init(argc, argv, "imu_to_vehicle_node");
 
-	
-	ros::NodeHandle n;
-
+	SubsriberPublisher sp;
+  	sp.sub = sp.n.subscribe("/data/imu", 1000, sp.imuCallback);
 
 	ros::Subscriber sub = n.subscribe("/data/imu", 1000, imuCallback);
 
 	ros::spin();
 
 	return 0;
+}
+
+class SubsriberPublisher
+{
+public:
+	SubsriberPublisher();
+	virtual ~SubsriberPublisher();
+	ros::Subscriber sub;
+	ros::Publisher  pub;
+	ros::NodeHandle n;
+	void imuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg);
+private:
+	/* data */
+};
+
+SubsriberPublisher::imuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg){
+	int a = 0;
 }
