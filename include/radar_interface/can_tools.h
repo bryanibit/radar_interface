ecb5cd30e0a53dab327c447eb2dc4be2d95ff981
Juraj Persic <juraj.persic@fer.hr>
2018-05-18 13:11:14 +0200

    parsing Can values WIP

--- include/radar_interface/can_tools.h
+++ include/radar_interface/can_tools.h
@@ -20,27 +20,52 @@ struct CANParseValueInfo {
 
 template <typename T>
 void setValueInFrame(can::Frame *frame, T &value,
-                     const CANParseValueInfo &parseInfo) {
+                     const CANParseValueInfo &parse_info) {
   // assumes that the frame slot is 0-filled
-  T value_clamped=std::max( T(parseInfo.MIN), std::min(value, T(parseInfo.MAX) ));
-  int int_value = (int((value_clamped - parseInfo.OFFSET) / parseInfo.SCALE))
-                  << parseInfo.SHIFT;
-  unsigned char temp,temp1,temp2,temp3,temp4,temp5;
-  for (size_t i = parseInfo.START_BYTE; i <= parseInfo.END_BYTE; i++) {
-    temp = (int_value >> (8 * (parseInfo.END_BYTE - i))) &
-           parseInfo.MASK[i];
-    temp1=        frame->data[i] & (~parseInfo.MASK[i]); 
-    temp2= frame->data[i];
-    temp3= (!parseInfo.MASK[i]);
-    frame->data[i] = frame->data[i] & (~parseInfo.MASK[i]);  
-    temp4=    frame->data[i];
-    temp5= frame->data[i] | temp;
+  T value_clamped =
+      std::max(T(parse_info.MIN), std::min(value, T(parse_info.MAX)));
+  int int_value = (int((value_clamped - parse_info.OFFSET) / parse_info.SCALE))
+                  << parse_info.SHIFT;
+  unsigned char temp, temp1, temp2, temp3, temp4, temp5;
+  for (size_t i = parse_info.START_BYTE; i <= parse_info.END_BYTE; i++) {
+    temp = (int_value >> (8 * (parse_info.END_BYTE - i))) & parse_info.MASK[i];
+    temp1 = frame->data[i] & (~parse_info.MASK[i]);
+    temp2 = frame->data[i];
+    temp3 = (!parse_info.MASK[i]);
+    frame->data[i] = frame->data[i] & (~parse_info.MASK[i]);
+    temp4 = frame->data[i];
+    temp5 = frame->data[i] | temp;
     frame->data[i] = frame->data[i] | temp;
-     
   }
 }
 
-}
+template <typename T>
+void parseValueFromFrame(const can::Frame &frame, T *value,
+                         const CANParseValueInfo &parse_info) {
+  // assumes that the frame slot is 0-filled
 
+  int int_value = 0;
+  unsigned char temp, temp1, temp2, temp3, temp4, temp5;
+  bool is_signed = std::is_signed<T>::value;
+
+  for (size_t i = parse_info.START_BYTE; i <= parse_info.END_BYTE; i++) {
+    temp = (int_value >> (8 * (parse_info.END_BYTE - i))) & parse_info.MASK[i];
+    temp1 = frame->data[i] & (~parse_info.MASK[i]);
+    temp2 = frame->data[i];
+    temp3 = (!parse_info.MASK[i]);
+    frame->data[i] = frame->data[i] & (~parse_info.MASK[i]);
+    temp4 = frame->data[i];
+    temp5 = frame->data[i] | temp;
+    frame->data[i] = frame->data[i] | temp;
+  }
+  for (size_t i = parse_info.START_BYTE; i <= parse_info.END_BYTE; i++) {
+    int_value = (int_value << 8) | (frame[i] & parse_info.MASK[i]);
+  }
+  &value = int_value * parse_info.SCALE;
+  if (is_signed && &value > parse_info.MAX) {
+    &value = &value + 2 * parse_info.MIN;
+  }
+}
+}
 
 #endif
